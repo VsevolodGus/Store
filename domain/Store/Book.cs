81b7c29fbf464144247f864af5a0cd0b1017953c
VsevolodGus <79750979+VsevolodGus@users.noreply.github.com>
2021-03-08 00:28:16 +0500

    Создал и добавил классы для хранения книг, заложил начало

---
+++ domain/Store/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Store
+{
+    public class Book
+    {
+        public uint Id { get; }
+        public string Title { get;  }
+
+        public Book(uint id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+    }
+}
